6e84bdaf604ad03b81957b53ca23b92067d5739d
DerSchinkenmann <97325240+DerSchinkenmann@users.noreply.github.com>
2026-09-03 08:38:20 -0400

    Add files via upload
    1st commit, update frequently

---
+++ 1st Period PGAS - C Wizzle/Mod2/Literals.py
@@ -0,0 +1,25 @@
+#Literals are data that are determined by the literal itself
+#123 is a literal, you can identify that it means one-hundred and twenty-three
+#c is not a literal, it can not be identified without additional knowledge
+
+
+print("2") 
+print(2)
+
+#these are examples of literals, the first is the string "2", the second is the integer 2
+
+print("2+2")
+print(2+2)
+#these are also literals
+#the difference is that py doesnt do math with the strings(the quotations)-
+#if it's an int, it will automatically add the two together
+
+#you can also make integers into octals(0-7) by adding an 0o prefix to the int-
+#or hexadecimal(0-15) by adding an 0x prefix to the int
+
+print(0o123) #octa
+#1 64(or 1*8^2), 2 8s(or 2*8^1), and 3 1s(or 3*8^0), which equals 83 
+print(0x123) #hexa
+#1 256(or 1*16^2), 2 16s(or 2*16^2), and 3 1s(or 3*16^0), which equals 291
+
+#the last example of a literal is boolean(t/f[1/0]) values
